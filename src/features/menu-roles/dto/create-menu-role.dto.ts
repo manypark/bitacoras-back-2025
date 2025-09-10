7de3a4616300c4cad691cc89d35eb997e09ff62e
Manu <manypark@live.com>
2025-09-10 10:00:29 -0600

    feat: add validation create menu rol

--- src/features/menu-roles/dto/create-menu-role.dto.ts
+++ src/features/menu-roles/dto/create-menu-role.dto.ts
@@ -1,8 +1,14 @@
 import { IsInt, IsNotEmpty } from 'class-validator';
+import { User } from 'src/features/auth/entities/user.entity';
 import { Menu } from 'src/features/menu/entities/menu.entity';
 import { Role } from 'src/features/roles/entities/role.entity';
 
 export class CreateMenuRoleDto {
+
+  @IsInt({ message: 'El idUser debe ser un número entero' })
+  @IsNotEmpty({ message: 'El idUser es requerido' })
+  idUser:User;
+
   @IsInt({ message: 'El idMenu debe ser un número entero' })
   @IsNotEmpty({ message: 'El idMenu es requerido' })
   idMenu:Menu;
