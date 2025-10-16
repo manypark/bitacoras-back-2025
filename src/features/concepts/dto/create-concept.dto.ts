9bb999c3063f37b8b6c0f72aa3ba89edcd00255d
Manu <manypark@live.com>
2025-10-16 08:34:20 -0600

    hotfix: add validation string name concept

--- src/features/concepts/dto/create-concept.dto.ts
+++ src/features/concepts/dto/create-concept.dto.ts
@@ -1,8 +1,10 @@
-import { IsString, IsBoolean, IsOptional } from 'class-validator';
+import { IsString, IsBoolean, IsOptional, IsNotEmpty, MinLength } from 'class-validator';
 
 export class CreateConceptDto {
     
   @IsString()
+  @IsNotEmpty({ message: 'El nombre del concepto es requerido' })
+  @MinLength(4, { message: 'El nombre debe tener al menos 4 caracteres' })
   description: string;
 
   @IsOptional()
